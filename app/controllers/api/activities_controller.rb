9e0b3d7a7d574d4e0b3db9fdf17343abb055cfd9
Caia Chuang <caiachuang@gmail.com>
2020-09-28 07:51:19 -0700

    Modify index action and add show action in the Activities controller for showing activities.

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,4 +1,12 @@
 class Api::ActivitiesController < ApplicationController
   def index
+    activities = Activity.all
+    render json: activities
+    # render json: ActivitySerializer.new(activities)
+  end
+  
+  def show
+    activity = Activity.find_by(id: params[:id])
+    render json: activity
   end
 end
