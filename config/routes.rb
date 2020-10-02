006554c4fa52b6cec8a8b4d8a269d54a8b08f5ae
Caia Chuang <caiachuang@gmail.com>
2020-10-02 01:18:27 -0700

    Create route for adding activity to bookings and favorites.

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,32 @@ Rails.application.routes.draw do
   
   
   namespace :api do # /api/data
-    
-    delete '/users/:user_id/bookings/:id', to: 'bookings#destroy'
-    delete '/users/:user_id/favorites/:id', to: 'favorites#destroy'
-    # get '/data', to: 'tests#index'
-    get '/activities/user/:user_id/booked', to: 'activities#user_booked_activities'
+    # URL: api/activities/user/:user_id/favored for favored activity
     get '/activities/user/:user_id/favored', to: 'activities#user_favored_activities'
+
+    # URL: api/activities/user/:user_id/booked for booked activity
+    get '/activities/user/:user_id/booked', to: 'activities#user_booked_activities'
+    
+    # URL: api/users/:user_id/bookings/:id for deleting booked activity
+    delete '/users/:user_id/bookings/:id', to: 'bookings#destroy'
+
+    # URL: api/users/:user_id/favorites/:id for deleting favored activity
+    delete '/users/:user_id/favorites/:id', to: 'favorites#destroy'
+    
+    # URL: api/users/:user_id/bookings for booking activity
+    post '/users/:user_id/bookings', to: 'bookings#create'
+    
+    # URL: api/users/:user_id/booking for adding activity to favorites 
+    post '/users/:user_id/favorites', to: 'favorites#create'
+
+    
     resources :activities 
 
     resources :users do
       resources :bookings
       resources :favorites
     end
+    # get '/data', to: 'tests#index'
 
   end
 
