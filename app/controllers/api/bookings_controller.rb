6df32d72cec27279998cebf0c7afa0724cc00969
Caia Chuang <caiachuang@gmail.com>
2020-10-02 01:20:02 -0700

    Add create action in bookings and favorites controller.

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,6 +1,5 @@
 class Api::BookingsController < ApplicationController
   def index
-    # bookings = Booking.all
     bookings = Booking.where(user_id: 1)
     render json: bookings
   end
@@ -10,17 +9,19 @@ class Api::BookingsController < ApplicationController
     render json: booking
   end
 
+  def create
+    booking = Booking.new(booking_params)
+    booking.save
+  end
+
   def destroy
     booking = Booking.find(params[:id])  
     booking.destroy
   end
-  # def create
-  #   activity = Activity.finde_by(id: params[:activity_id])
-
-  #   booking = Booking.new(
-  #     number_of_participants: params[:number_of_participants]
-  #     price_per_person: activity.price_per_person 
-  #   )
-  # end
   
+  
+  private
+  def booking_params
+    params.require(:booking).permit(:user_id, :activity_id, :number_of_participants, :price_per_person)
+  end
 end
