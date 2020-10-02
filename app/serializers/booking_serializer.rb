bb1619cf4f09382279be0b4780cc6532ee7aaef7
Caia Chuang <caiachuang@gmail.com>
2020-10-02 01:17:16 -0700

    Modify booking serializer.

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,3 +1,3 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :activity_id
+  attributes :id, :user_id, :activity_id, :number_of_participants, :price_per_person
 end
