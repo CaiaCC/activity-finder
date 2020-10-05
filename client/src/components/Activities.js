0c0ebbc8e7b8124f4e3e8c1966a2e98363f7567c
Jeffrey Shao <shaoyuhao97090@gmail.com>
2020-10-05 06:22:48 +0000

    unused components are removed

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Button } from 'react-bootstrap'
 import { Dropdown } from 'react-bootstrap'
 import Banner from './Banner'
-import { Button, CardColumns, Container, Spinner, Alert } from 'react-bootstrap'
+import { Button, CardColumns, Container } from 'react-bootstrap'
 
 function Activities(props) {
 
